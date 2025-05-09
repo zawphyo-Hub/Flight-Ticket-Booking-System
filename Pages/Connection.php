a6f684c86269fe3c4b8512e390e10c10fe83d2c5
Ryan <zawminp2003@gmail.com>
2025-05-09 12:15:44 +0630

    Update Connection.php
    Added Port = 3307, which is because i change my mysql in xampp pannel from 3306 to 3307.

--- Pages/Connection.php
+++ Pages/Connection.php
@@ -3,7 +3,8 @@
     $username = "root";
     $password = "";
     $dbname = "saewebsite";
+    $port = 3307;
 
-    $connection = mysqli_connect($host,$username,$password,$dbname);
+    $connection = mysqli_connect($host,$username,$password,$dbname,$port);
 
 ?>
